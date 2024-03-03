392551f892a3b7660b98fdf976f5b94682b1f0a8
= <=>
2024-03-04 01:10:34 +0530

    re-installation

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
+const path = require("path");
+
 module.exports = {
   env: {
     NEXT_API_KEY: process.env.NEXT_API_KEY,
     BASE_URL: process.env.BASE_URL,
   },
-};
-
-const path = require("path");
-
-module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
