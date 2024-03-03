a791a80e7ba0579e2c6d053b6fcf497a83cbc2d3
= <=>
2024-03-04 00:59:04 +0530

    sass explicit config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-    env: {
-      NEXT_API_KEY: process.env.NEXT_API_KEY,
-      BASE_URL: process.env.BASE_URL,
-    },
-  };
+  env: {
+    NEXT_API_KEY: process.env.NEXT_API_KEY,
+    BASE_URL: process.env.BASE_URL,
+  },
+};
+
+const path = require("path");
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
